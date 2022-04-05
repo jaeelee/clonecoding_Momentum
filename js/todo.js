ddc02f1c6b57ddac4692b3bc16fcd61aecb5c788
jaeelee <lje3896@gmail.com>
2022-04-06 01:43:12 +0900

    ADD: todo list 삭제 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -1,18 +1,22 @@
+const TODOS_KEY = "todos"
+
 const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
-const todos = [];
+let todos = [];
 
 function saveTodos()
 {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos)); // JSON.stringify : array를 string으로 변환
 }
 
 function deleteTodo(event)
 {
     const li = event.target.parentElement;
     li.remove();
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+    saveTodos();
 }
 
 function paintTodo(newTodo)
@@ -21,7 +25,8 @@ function paintTodo(newTodo)
     const span = document.createElement("span");
     const button = document.createElement("button");
 
-    span.innerText = newTodo;
+    li.id = newTodo.id;
+    span.innerText = newTodo.text;
     button.innerText = "❌";
     button.addEventListener("click", deleteTodo);
 
@@ -33,10 +38,23 @@ function paintTodo(newTodo)
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
-    paintTodo(newTodo);
-    todos.push(newTodo);
+    const nnewTodoObj = {
+        text: newTodo,
+        id: Date.now()
+    }
     todoInput.value = "";
+    todos.push(nnewTodoObj);
+    paintTodo(nnewTodoObj);
     saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null)
+{
+    const parsedTodos = JSON.parse(savedTodos); // JSON.parse : string을 javascript object로 변환
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
